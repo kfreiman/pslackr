f0d2c1899a9b73926151544f3a103699ac7f47dd
Kirill Freiman <k.freiman@ya.ru>
2015-10-13 14:25:02 +0300

    change package name

---
+++ examples/index.php
@@ -0,0 +1,11 @@
+<?php
+
+// namespace Test;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+$slack = new FullyBaked\Pslackr\Pslackr('https://hooks.slack.com/services/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');
+$message = new FullyBaked\Pslackr\Messages\CustomMessage('text');
+
+$slack->send($message);
